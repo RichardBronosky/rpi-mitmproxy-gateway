4aa6371f0546731e28f57755d81da464a91326c7
Richard Bronosky <richard@bronosky.com>
2013-02-14 17:00:51 -0500

    No need for ruby

--- slim.sh
+++ slim.sh
@@ -10,12 +10,9 @@ apt-get -y remove --purge raspberrypi-artwork
 apt-get -y remove --purge omxplayer
 apt-get -y autoremove
 apt-get -y update
-apt-get -y install ruby
-apt-get -y install ruby1.9.1-dev
 apt-get -y install git
 apt-get -y install libnss-mdns
 apt-get -y install vim
-gem install pi_piper
 rm -rf /home/pi/python_games
 wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update && sudo chmod +x /usr/bin/rpi-update
 rpi-update
