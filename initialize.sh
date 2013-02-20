33fbbf4dfc10874646bbf578f0a71fdfea7632b6
Richard Bronosky <richard@bronosky.com>
2013-02-19 22:05:38 -0500

    Add standard bash preamble

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
+## No bash script should be considered releasable until it has this! ##
+# Exit on use of an uninitialized variable
+set -o nounset
+# Exit if any statement returns a non-true return value (non-zero).
+set -o errexit
+
 # Remove cruft packages
 apt-get -y remove --purge xserver-common
 apt-get -y remove --purge x11-common
